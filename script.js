b542542667062f8c5197a1f019f1b32cc8fd5751
vvxiim <maximguts1@gmail.com>
2026-02-02 00:15:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-document.getElementById("refForm").addEventListener("submit", function(e) {
+document.getElementById("refForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const data = {
-        fio: document.getElementById("fio").value,
-        username: document.getElementById("username").value
-    };
+    const fio = document.getElementById("fio").value;
+    const username = document.getElementById("username").value;
+
+    const formData = new URLSearchParams();
+    formData.append("fio", fio);
+    formData.append("username", username);
 
     fetch("https://script.google.com/macros/s/AKfycbzv2ZQUIsuanOIdR5O-iUjm0iknaKBpxM8IkZVuZD5d49w9x7VwjE9QSVAebzZQ1ts/exec", {
         method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(data)
+        body: formData
     })
-    .then(res => res.text())
     .then(() => {
         window.location.href = "https://b24-kn381m.b24site.online/crm_form_iemti/";
     })
     .catch(err => {
-        console.error(err);
-        alert("Произошла ошибка при отправке. Попробуйте ещё раз.");
+        console.error("Ошибка отправки:", err);
+        alert("Ошибка отправки данных");
     });
 });
+
 
 
 
