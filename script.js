802eaf7154f36b7d0df5890f85cea174c7ed0417
vvxiim <maximguts1@gmail.com>
2026-02-01 23:55:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,25 +6,22 @@ document.getElementById("refForm").addEventListener("submit", function(e) {
         username: document.getElementById("username").value
     };
 
-    const scriptURL = "https://script.google.com/macros/s/AKfycby3LeR92M0YjfWMzkSVQWEWQMCBhCg94NVLahNBLDUieOEEoV_5rlYO4Z-BJkAGtac/exec";
-
-    // Отправляем данные как form-urlencoded
-    fetch(scriptURL, {
+    fetch("https://script.google.com/macros/s/AKfycbzv2ZQUIsuanOIdR5O-iUjm0iknaKBpxM8IkZVuZD5d49w9x7VwjE9QSVAebzZQ1ts/exec", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(data)
     })
-    .then(response => response.text())
-    .then(result => {
-        console.log("Данные успешно отправлены:", result);
-        // Перенаправление только после успешной отправки
+    .then(res => res.text())
+    .then(() => {
         window.location.href = "https://b24-kn381m.b24site.online/crm_form_iemti/";
     })
-    .catch(error => {
-        console.error("Ошибка отправки:", error);
+    .catch(err => {
+        console.error(err);
         alert("Произошла ошибка при отправке. Попробуйте ещё раз.");
     });
 });
+
+
 
 
 
